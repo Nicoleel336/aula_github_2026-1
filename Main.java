28e86ced7db48a4f70a6e0b054008d2d95bf5382
nicole <nicole.silvestre@ufpi.edu.br>
2026-05-27 21:10:50 -0300

    Implementação consulta por clientes. Closes #2

--- Main.java
+++ Main.java
@@ -4,26 +4,40 @@ public class Main {
     public static void main(String[] args) {
         ClienteService clienteService = new ClienteService();
 
-        Menu mainMenu = new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes"));
-        int opcaoSelecionada = mainMenu.getSelection();
+        boolean executando = true;
 
-        if (opcaoSelecionada == 1) {
-            System.out.println("Modulo Conta ainda nao implementado.");
-        } else if (opcaoSelecionada == 2) {
-            Menu clienteMenu = new Menu("Menu Cliente", Arrays.asList("Cadastrar Cliente"));
-            int opcaoCliente = clienteMenu.getSelection();
+        while (executando) {
+            Menu mainMenu = new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes", "Sair"));
+            int opcaoSelecionada = mainMenu.getSelection();
 
-            if (opcaoCliente == 1) {
-                clienteService.cadastrarCliente();
+            if (opcaoSelecionada == 1) {
+                System.out.println("Modulo Conta ainda nao implementado.");
+            } else if (opcaoSelecionada == 2) {
+                executarMenuCliente(clienteService);
+            } else if (opcaoSelecionada == 3) {
+                System.out.println("Modulo Operacoes ainda nao implementado.");
+            } else if (opcaoSelecionada == 4) {
+                executando = false;
             } else {
                 System.out.println("Opcao invalida.");
             }
-        } else if (opcaoSelecionada == 3) {
-            System.out.println("Modulo Operacoes ainda nao implementado.");
-        } else {
-            System.out.println("Opcao invalida.");
         }
 
         System.out.println("Fim");
+    }
+
+    private static void executarMenuCliente(ClienteService clienteService) {
+        Menu clienteMenu = new Menu("Menu Cliente", Arrays.asList("Cadastrar Cliente", "Consultar Cliente", "Voltar"));
+        int opcaoCliente = clienteMenu.getSelection();
+
+        if (opcaoCliente == 1) {
+            clienteService.cadastrarCliente();
+        } else if (opcaoCliente == 2) {
+            clienteService.consultarCliente();
+        } else if (opcaoCliente == 3) {
+            System.out.println("Retornando ao menu principal.");
+        } else {
+            System.out.println("Opcao invalida.");
+        }
     }
 }
